40b724ca80c335b7051ce33a033a1ab4527487b6
Jimikumar Chhatrala <jimichhatrala8488@gmail.com>
2022-08-02 06:14:16 +0530

    App UI Designed Code

---
+++ lib/on_generate_route.dart
@@ -0,0 +1,71 @@
+
+
+
+import 'package:flutter/material.dart';
+import 'package:flutter/cupertino.dart';
+import 'package:notes_app_2/app_const.dart';
+import 'package:notes_app_2/feature/domain/entities/note_entity.dart';
+import 'package:notes_app_2/feature/presentation/pages/add_new_note_page.dart';
+import 'package:notes_app_2/feature/presentation/pages/sign_in_page.dart';
+import 'package:notes_app_2/feature/presentation/pages/sign_up_page.dart';
+import 'package:notes_app_2/feature/presentation/pages/update_note_page.dart';
+
+class OnGenerateRoute{
+  static Route<dynamic> route(RouteSettings settings){
+    final args=settings.arguments;
+
+
+    switch(settings.name){
+      case PageConst.signInPage:{
+        return materialBuilder(widget: SignInPage());
+        break;
+      }
+      case PageConst.signUpPage:{
+        return materialBuilder(widget: SignUpPage());
+        break;
+      }
+      case PageConst.addNotePage:{
+        if (args is String) {
+          return materialBuilder(widget: AddNewNotePage(uid: args,));
+        }
+        else {
+          return materialBuilder(
+            widget: ErrorPage(),
+          );
+        }
+        break;
+      }
+      case PageConst.UpdateNotePage:{
+        if (args is NoteEntity) {
+          return materialBuilder(widget: UpdateNotePage(noteEntity: args,));
+        }
+        else {
+          return materialBuilder(
+            widget: ErrorPage(),
+          );
+        }
+        break;
+      }
+      default: return materialBuilder(widget: ErrorPage());
+    }
+
+  }
+}
+
+class ErrorPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text("error"),
+      ),
+      body: Center(
+        child: Text("error"),
+      ),
+    );
+  }
+}
+
+MaterialPageRoute materialBuilder({required Widget widget}) {
+  return MaterialPageRoute(builder: (_) => widget);
+}
